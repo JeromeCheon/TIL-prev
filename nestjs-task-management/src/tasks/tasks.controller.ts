314f1d94dc19e68fd36d8d863a0dcd887c37b64b
Jerome <cheonjh4572@gmail.com>
2021-09-29 00:48:07 +0900

    Add createTask method in service

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   // 이렇게 파라미터를 넣어주면 자동으로 위에 생성이 돼
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) { }
+  // controller에 다음과 같은 handler를 넣어주는 거야
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+
 }
--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+
 @Injectable()
 export class TasksService {
   private tasks: Task[] = []; // 이렇게 Task 타입의 array라는 것 명시
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      // 자 이제 id를 어떻겍 auto generate 시켜줄까? 다양한 방법이 있겠지만
+      // 여기서 우린 uuid라는 걸 install 해서 써보자
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    // 생성하니까 우리 task 리스트에 먼저 넣어주고, 리턴해
+    this.tasks.push(task);
+    return task;
   }
 }
