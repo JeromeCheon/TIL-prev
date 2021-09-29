95187b0f6351ca52434cbee96adada1b90919cf0
Jerome <cheonjh4572@gmail.com>
2021-09-29 23:47:52 +0900

    Update prevent empty values using class-validator and transformer

--- nestjs-task-management/src/tasks/dto/create-task.dto.ts
+++ nestjs-task-management/src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,9 @@
+import { IsNotEmpty } from 'class-validator'; // value 빈걸 방지하기 위해
+
 export class CreateTaskDto {
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
   // 여기에 다른 변수를 추가해도 다른 코드 손댈 필요가 없어
 }
--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { title } from 'process';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
