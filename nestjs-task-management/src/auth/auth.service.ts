a56caed45fa80884bd1b9601c8edabaefecce23b
Jerome <cheonjh4572@gmail.com>
2021-11-07 20:47:36 +0900

    Add sign up method

--- nestjs-task-management/src/auth/auth.service.ts
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,4 +9,8 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
--- nestjs-task-management/src/auth/users.repository.ts
+++ nestjs-task-management/src/auth/users.repository.ts
@@ -1,7 +1,13 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  // nothing in here for now
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
 }
