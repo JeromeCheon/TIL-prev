0798b121de7d52371508e4e5f4d29452bc686b03
Jerome <cheonjh4572@gmail.com>
2021-11-07 14:38:57 +0900

    Update properties and packages version with new laptop

--- nestjs-task-management/src/app.module.ts
+++ nestjs-task-management/src/app.module.ts
@@ -1,7 +1,20 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
