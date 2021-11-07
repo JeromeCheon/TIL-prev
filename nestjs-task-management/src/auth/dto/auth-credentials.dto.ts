ba67637487a0a77d56aded30e79f80f566d34861
Jerome <cheonjh4572@gmail.com>
2021-11-07 20:55:04 +0900

    Update auth-credential with regular expression class validation logic

--- nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
+++ nestjs-task-management/src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak', // 메시지를 커스터마이징 하고 싶다면 이렇게 두번째 인자로 줄 수 있어. optional
+  })
   password: string;
 }
