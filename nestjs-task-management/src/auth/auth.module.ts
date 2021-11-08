73493e6ec4539b6cbd5a26b98726d2bfcbd82d73
Jerome <cheonjh4572@gmail.com>
2021-11-09 00:32:37 +0900

    Add jwtmodule and passport module to signing in later

--- nestjs-task-management/src/auth/auth.module.ts
+++ nestjs-task-management/src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600, // 1 hour
+      },
+    }),
+    TypeOrmModule.forFeature([UsersRepository]),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
