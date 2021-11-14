20cb30a77f257b615ee55e9c90cd733e5c43dcc2
Jerome <cheonjh4572@gmail.com>
2021-11-14 23:54:19 +0900

    Update main file with 11/13 study feedback about questions

--- nestjs-task-management/src/main.ts
+++ nestjs-task-management/src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// 11/13 피드백 내용: nestjs/common의 내용들은 자주 쓰는 것만 알아둘 필요가 있다.
+// 필요할 땐 공식 docs 보고 바로 찾을 수 있도록 훈련이 필요함.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
