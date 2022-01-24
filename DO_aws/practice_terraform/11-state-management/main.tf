aac0dce0f7780b46108e901944818fa56d8a582a
Jerome <cheonjh4572@gmail.com>
2022-01-24 16:54:32 +0900

    Add state management files to connect tf cloud remote from fastcampus, but tfvars doesn't work

---
+++ DO_aws/practice_terraform/11-state-management/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "jerome-test"
+
+    workspaces {
+      name = "tf-cloud-backend"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+/* GROUP */
+resource "aws_iam_group" "this" {
+  for_each = toset(["developer", "employee"])
+
+  name = each.key
+}
+
+output "groups" {
+  value = aws_iam_group.this
+}
+
+/* USERS */
+variable "users" {
+  type = list(any)
+}
+
+resource "aws_iam_user" "this" {
+  for_each = {
+    for user in var.users :
+    user.name => user
+  }
+
+  name = each.key
+
+  tags = {
+    level = each.value.level
+    role  = each.value.role
+  }
+}
+
+resource "aws_iam_user_group_membership" "this" {
+  for_each = {
+    for user in var.users :
+    user.name => user
+  }
+
+  user   = each.key
+  groups = each.value.is_developer ? [aws_iam_group.this["developer"].name, aws_iam_group.this["employee"].name] : [aws_iam_group.this["employee"].name]
+}
+
+locals {
+  developers = [
+    for user in var.users :
+    user
+    if user.is_developer
+  ]
+}
+
+output "developers" {
+  value = local.developers
+}
+
+output "high_level_users" {
+  value = [
+    for user in var.users :
+    user
+    if user.level > 5
+  ]
+}
+
