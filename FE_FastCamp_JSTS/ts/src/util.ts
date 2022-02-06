a9c3731f4fe2bf40d9c2170296c97d94a31cef9b
Jerome <cheonjh4572@gmail.com>
2022-02-06 23:00:03 +0900

    [FE_TS] Update util file by inserting noImplicitAny option in tsconfig

--- FE_FastCamp_JSTS/ts/src/util.ts
+++ FE_FastCamp_JSTS/ts/src/util.ts
@@ -1,8 +1,8 @@
 // 주어진 두 파라미터의 합을 구하는 함수
-export default function add(a, b) {
+export default function add(a: number, b: number): number {
   return a + b;
 }
 
-export function minus(a, b) {
+export function minus(a: number, b: number): number {
   return a - b;
 }
