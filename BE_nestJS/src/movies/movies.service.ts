3d1270a88d4119e893003e87359609639648b7c3
Dev. Jerome <cheonjh4572@gmail.com>
2022-02-10 23:49:59 +0900

    [BE_nestJS] Updatte movie service getOne method with NotfoundException when input is invalid case

--- BE_nestJS/src/movies/movies.service.ts
+++ BE_nestJS/src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,12 +10,16 @@ export class MoviesService {
   }
 
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Moive with ID ${id} not found`);
+    }
+    return movie;
   }
 
-  deleteOne(id: string): boolean {
+  deleteOne(id: string) {
+    this.getOne(id);
     this.movies.filter((movie) => movie.id !== +id);
-    return true;
   }
 
   create(movieData) {
