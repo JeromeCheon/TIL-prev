9416df533c7e91ffe161a812c48a6ed96eafcc17
Jerome <cheonjh4572@gmail.com>
2022-02-10 14:04:17 +0900

    [BE_nestJS] Add getAll method in MoviesController

--- BE_nestJS/src/movies/movies.controller.ts
+++ BE_nestJS/src/movies/movies.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get()
+  getAll(): string {
+    return 'This will return all movies';
+  }
+}
