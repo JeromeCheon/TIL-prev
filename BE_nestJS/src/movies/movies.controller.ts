527d34dc4c81e2e22b7ef2691319a1846bc7696f
Jerome <cheonjh4572@gmail.com>
2022-02-10 17:58:49 +0900

    [BE_nestJS] Add movie entity and service then connect to controller

--- BE_nestJS/src/movies/movies.controller.ts
+++ BE_nestJS/src/movies/movies.controller.ts
@@ -6,27 +6,37 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll(): string {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after: ${searchingYear}`;
+  // }
+
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie of ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.create(movieData);
   }
   @Delete(':id')
   delete(@Param('id') movieId: string) {
-    return `This will delete a movie of ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
   @Patch(':id')
   patch(@Param('id') movieId: string, @Body() updateData) {
