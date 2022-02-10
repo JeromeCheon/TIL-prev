d5ca70209a78f813ab9c6ab4b84e3f6d546c6b21
Dev. Jerome <cheonjh4572@gmail.com>
2022-02-10 23:55:45 +0900

    [BE_nestJS] Update movie service deleteOne to reflect change and Add update method without validation for now

--- BE_nestJS/src/movies/movies.service.ts
+++ BE_nestJS/src/movies/movies.service.ts
@@ -19,13 +19,22 @@ export class MoviesService {
 
   deleteOne(id: string) {
     this.getOne(id);
-    this.movies.filter((movie) => movie.id !== +id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
   }
 
   create(movieData) {
     this.movies.push({
       id: this.movies.length + 1,
       ...movieData,
+    });
+  }
+
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({
+      ...movie,
+      ...updateData,
     });
   }
 }
