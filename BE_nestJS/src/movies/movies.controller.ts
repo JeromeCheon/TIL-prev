c0e532915cf037b491ab405512b3cd89b1ccd0a5
Jerome <cheonjh4572@gmail.com>
2022-02-13 13:16:41 +0900

    [BE_nestJS] Update moviescontroller with comments about explanation of @Req, @Res used in express that would be worked

--- BE_nestJS/src/movies/movies.controller.ts
+++ BE_nestJS/src/movies/movies.controller.ts
@@ -7,6 +7,8 @@ import {
   Patch,
   Post,
   Query,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
@@ -18,6 +20,8 @@ export class MoviesController {
 
   @Get()
   getAll(): Movie[] {
+    // getAll(@Req() req, @Res() res): Movie[] { // 이렇게 하는 것이 express 방식.
+    // 동작은 하나, 권하지 않는다. nestJS는 express와 fastify 두가지를 사용하기 때문에 express 하나만으로 국한시키지 말자
     return this.moviesService.getAll();
   }
 
