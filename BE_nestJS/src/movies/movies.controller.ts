76a012f3f65da314440da9e857bcdc33eae0eecb
Jerome <cheonjh4572@gmail.com>
2022-02-14 23:57:27 +0900

    [BE_nestJS] e2e testing and all of practice in movies app from Nomad coder lecture

--- BE_nestJS/src/movies/movies.controller.ts
+++ BE_nestJS/src/movies/movies.controller.ts
@@ -6,11 +6,9 @@ import {
   Param,
   Patch,
   Post,
-  Query,
-  Req,
-  Res,
 } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -24,12 +22,6 @@ export class MoviesController {
     // 동작은 하나, 권하지 않는다. nestJS는 express와 fastify 두가지를 사용하기 때문에 express 하나만으로 국한시키지 말자
     return this.moviesService.getAll();
   }
-
-  // @Get('search')
-  // search(@Query('year') searchingYear: string) {
-  //   return `We are searching for a movie made after: ${searchingYear}`;
-  // }
-
   @Get(':id')
   getOne(@Param('id') movieId: number): Movie {
     return this.moviesService.getOne(movieId);
@@ -44,10 +36,7 @@ export class MoviesController {
     return this.moviesService.deleteOne(movieId);
   }
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
-    return {
-      updatedMovie: movieId,
-      ...updateData,
-    };
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
   }
 }
--- BE_nestJS/test/app.e2e-spec.ts
+++ BE_nestJS/test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -12,6 +12,16 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    // 역기 보면 app을 생성하더라도 기존 앱처럼 pipe를 생성해서 사용하지 않고 있다.(transform)
+    // 따라서 id 값을 받아도 number가 아닌 string으로 출력되며 일반적으로 expect 하더라도 오류가 뜨는것
+    // 즉, 실제 돌아가는 환경하고 테스트 환경을 맞춰줘야 한다.
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -27,7 +37,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -38,13 +48,36 @@ describe('AppController (e2e)', () => {
         .expect(201);
     });
 
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
   });
-  describe('/movies:id', () => {
-    it.todo('GET');
-    it.todo('DELETE');
-    it.todo('PATCH');
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
