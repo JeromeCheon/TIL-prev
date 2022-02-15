0d7a6244268a251c3cc9d3f5f53f3db833730e5e
Jerome <cheonjh4572@gmail.com>
2022-02-15 22:43:17 +0900

    [BE_express-TDD] Add jest.fn to test function

--- BE_express/todo-tdd/controllers/todo.controller.ts
+++ BE_express/todo-tdd/controllers/todo.controller.ts
@@ -1,1 +1,5 @@
-exports.createTodo = () => {};
+const TodoModel = require('../model/todo.model')
+
+exports.createTodo = (req, res, next) => {
+	TodoModel.create()
+}
--- BE_express/todo-tdd/tests/unit/todo.controller.test.js
+++ BE_express/todo-tdd/tests/unit/todo.controller.test.js
@@ -1,7 +1,15 @@
-const TodoController = require('../../controllers/todo.controller');
+const TodoController = require('../../controllers/todo.controller')
+const TodoModel = require('../../model/todo.model')
+
+TodoModel.create = jest.fn()
 
 describe('TodoController.createTodo', () => {
 	it('should have a createTodo function', () => {
-		expect(typeof TodoController.createTodo).toBe('function');
-	});
-});
+		expect(typeof TodoController.createTodo).toBe('function')
+	})
+
+	it('should call TodoModel.create', () => {
+		TodoController.createTodo()
+		expect(TodoModel.create).toBeCalled()
+	})
+})
