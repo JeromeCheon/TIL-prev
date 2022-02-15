fe1ed77677c428f41b01eec96fe5a01ecee3feca
Jerome <cheonjh4572@gmail.com>
2022-02-15 23:16:13 +0900

    [BE_express-TDD] Update todo controller and its test file with req, res body

--- BE_express/todo-tdd/tests/unit/todo.controller.test.js
+++ BE_express/todo-tdd/tests/unit/todo.controller.test.js
@@ -1,15 +1,24 @@
 const TodoController = require('../../controllers/todo.controller')
 const TodoModel = require('../../model/todo.model')
+const httpMocks = require('node-mocks-http')
+const newTodo = require('../mock-data/new-todo.json')
 
 TodoModel.create = jest.fn()
+
+let req, res, next
+beforeEach(() => {
+	req = httpMocks.createRequest()
+	res = httpMocks.createResponse()
+	next = null
+})
 
 describe('TodoController.createTodo', () => {
 	it('should have a createTodo function', () => {
 		expect(typeof TodoController.createTodo).toBe('function')
 	})
-
 	it('should call TodoModel.create', () => {
-		TodoController.createTodo()
-		expect(TodoModel.create).toBeCalled()
+		req.body = newTodo
+		TodoController.createTodo(req, res, next)
+		expect(TodoModel.create).toBeCalled(newTodo)
 	})
 })
