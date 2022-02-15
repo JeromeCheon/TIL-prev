14877760d69bf3dda798603bd7da2d35492a9b67
Jerome <cheonjh4572@gmail.com>
2022-02-16 00:33:50 +0900

    [BE_express-TDD] Add express router and use it in app.js before debug test

--- BE_express/todo-tdd/app.js
+++ BE_express/todo-tdd/app.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const todoRoutes = require('./routes/todo.routes')
+const app = express()
+
+app.use('/todos', todoRoutes) // CRUD에 사용될 것이다 라는 의미
 
 app.get('/', (req, res) => {
-	res.send('Hello world~!');
-});
+	res.send('Hello world~!')
+})
 
 app.listen(3000, () => {
-	console.log('Server is now running!');
-});
+	console.log('Server is now running!')
+})
+
+module.exports = app
