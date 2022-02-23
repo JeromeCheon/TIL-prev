d21cd3b641d016f4e613e730f68f420b12f8c45f
Jerome <cheonjh4572@gmail.com>
2022-02-23 14:21:18 +0900

    [BE_Go] Update Urlchecker with error handling logic and status code

--- BE_Go/urlchecker/main.go
+++ BE_Go/urlchecker/main.go
@@ -9,6 +9,8 @@ import (
 var errRequestFailed = errors.New("Request failed")
 
 func main()  {
+	// var results = map[string]string{}  // 이렇게 {}를 붙여서 map에 대한 인스턴스를 만들거나
+	var results = make(map[string]string) // 이런식으로 empty map으로 초기화할 수 있어
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -19,16 +21,23 @@ func main()  {
 		"https://www.instagram.com/",
 		"https://academy.nomadcoders.co/",
 	}
-
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
 func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
 		return errRequestFailed
 	}
 	return nil
