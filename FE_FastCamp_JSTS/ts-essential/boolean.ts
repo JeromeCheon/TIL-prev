3a6fa2a296333775f352a370d715c40d5f058a1c
Jerome <cheonjh4572@gmail.com>
2022-02-23 23:32:01 +0900

    [FE_TS] Add boolean practice and tsconfig customization following TS-essential lecture

---
+++ FE_FastCamp_JSTS/ts-essential/boolean.ts
@@ -0,0 +1,9 @@
+let isDone: boolean = false;
+
+isDone = true;
+
+console.log(typeof isDone); // 'boolean'
+
+let isOk: Boolean = true;
+
+// let isNotOk: boolean = new Boolean(true); // 이렇게 객체를 primitive type에 넣을 수 없다
