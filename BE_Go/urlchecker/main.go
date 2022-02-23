32d4d69614aa6f68829264083fc69d4cc328b2c0
Jerome <cheonjh4572@gmail.com>
2022-02-23 16:06:13 +0900

    [BE_Go] Add go routine and channel practice

--- BE_Go/urlchecker/main.go
+++ BE_Go/urlchecker/main.go
@@ -4,34 +4,44 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 var errRequestFailed = errors.New("Request failed")
 
 func main()  {
 	// var results = map[string]string{}  // 이렇게 {}를 붙여서 map에 대한 인스턴스를 만들거나
-	var results = make(map[string]string) // 이런식으로 empty map으로 초기화할 수 있어
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
+	// var results = make(map[string]string) // 이런식으로 empty map으로 초기화할 수 있어
+	// urls := []string{
+	// 	"https://www.airbnb.com/",
+	// 	"https://www.google.com/",
+	// 	"https://www.amazon.com/",
+	// 	"https://www.reddit.com/",
+	// 	"https://soundcloud.com/",
+	// 	"https://www.facebook.com/",
+	// 	"https://www.instagram.com/",
+	// 	"https://academy.nomadcoders.co/",
+	// }
+	// for _, url := range urls {
+	// 	result := "OK"
+	// 	err := hitURL(url)
+	// 	if err != nil {
+	// 		result = "FAILED"
+	// 	}
+	// 	results[url] = result
+	// }
+	// for url, result := range results {
+	// 	fmt.Println(url, result)
+	// }
+	
+	// go routine과 channel 학습
+	c := make(chan bool)
+	people := [2]string{"jerome", "simo"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
 func hitURL(url string) error {
@@ -41,4 +51,10 @@ func hitURL(url string) error {
 		return errRequestFailed
 	}
 	return nil
+}
+
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
