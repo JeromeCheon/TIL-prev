0b6af7fdb99df66fb1804281855d7e1bdd2192fa
Jerome <cheonjh4572@gmail.com>
2022-02-23 23:42:10 +0900

    [FE_TS] Add number type practice and skip string practice in code, but wrote in notion

---
+++ FE_FastCamp_JSTS/ts-essential/number.ts
@@ -0,0 +1,11 @@
+let decimal: number = 6;
+
+let hex: number = 0xf00d;
+
+let binary: number = 0b1010;
+
+let octal: number = 0o744;
+
+let notANumber: number = NaN;
+
+let underscoreNum: number = 1_000_000;
