61b51b8b3cc7cef500f4e7f971f11f140db90da9
Jerome <cheonjh4572@gmail.com>
2022-02-27 23:51:22 +0900

    [FE_TS] Update interface1 practice with optional property

--- FE_FastCamp_JSTS/ts-essential/interface1.ts
+++ FE_FastCamp_JSTS/ts-essential/interface1.ts
@@ -1,7 +1,8 @@
 interface Person1 {
 	// 이렇게 Person 쪽을 따로 빼서 만들 수 있다
 	name: string;
-	age: number;
+	age?: number; // optional 하게 받을 수 있다.
+	[index: string]: any; // 어떤 이름의 property가 와도 괜찮아
 }
 
 function hello1(person: Person1): void {
@@ -14,3 +15,14 @@ const p1: Person1 = {
 };
 
 hello1(p1);
+
+const p32: Person1 = {
+	name: 'Anna',
+	sisters: ['Sung', 'Chan'],
+};
+
+const p33: Person1 = {
+	name: 'jerome',
+	father: p1,
+	mother: p32,
+};
