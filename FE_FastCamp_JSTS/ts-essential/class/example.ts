c65d8ac7a379ed83a672ce2022d27e42dd3618bb
Jerome <cheonjh4572@gmail.com>
2022-03-01 14:23:27 +0900

    [FE_TS] Update class example with getter and setter

--- FE_FastCamp_JSTS/ts-essential/class/example.ts
+++ FE_FastCamp_JSTS/ts-essential/class/example.ts
@@ -2,8 +2,17 @@ class Person2 {
 	// 보통 여기에 property를 놓고 생성자에서 this.name = name 이렇게 할텐데
 	// 여기 매개변수에서 다음과 같이 접근제어자를 같이 명시해서 코드를 단축시킬 수도 있다
 	// 많이 쓰는 방법이니 참고해두자
-	public constructor(public name: string, public age: number) {}
+	public constructor(private _name: string, public age: number) {}
+	get name() {
+		console.log('get');
+		return this._name;
+	}
+	set name(n: string) {
+		this._name = n;
+	}
 }
 
 const p22: Person2 = new Person2('Jerome', 28);
+console.log(p22.name, p22.age); // get을 하는 함수 getter
+p22.name = 'jerome'; // set을 하는 함수 setter
 console.log(p22.name, p22.age);
