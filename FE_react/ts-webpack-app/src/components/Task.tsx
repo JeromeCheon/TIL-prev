5fd42525154b48edd7d5bf1977fffaf65836fbdf
Jerome <cheonjh4572@gmail.com>
2022-03-16 08:56:19 +0900

    [FE_React] Update Task.tsx to use state and relative task functions receiving through input parameters

--- FE_react/ts-webpack-app/src/components/Task.tsx
+++ FE_react/ts-webpack-app/src/components/Task.tsx
@@ -6,8 +6,31 @@ const Task = ({
 	onPinTask,
 }): JSX.Element => {
 	return (
-		<div className='list-item'>
-			<input type='text' value={title} readOnly={true} />
+		<div className={`list-item ${state}`}>
+			<label className='checkbox'>
+				<input
+					type='checkbox'
+					defaultChecked={state === 'TASK_ARCHIVED'}
+					disabled={true}
+					name='checked'
+				/>
+				<span className='checkbox-custom' onClick={() => onArchiveTask(id)} />
+			</label>
+			<div className='title'>
+				<input
+					type='text'
+					value={title}
+					readOnly={true}
+					placeholder='Input title'
+				/>
+			</div>
+			<div className='actions' onClick={(event) => event.stopPropagation()}>
+				{state !== 'TASK_ARCHIVED' && ( // eslint-disable-next-line jsx-a11y/anchor-is-valid
+					<a onClick={() => onPinTask(id)}>
+						<span className={`icon-star`} />
+					</a>
+				)}
+			</div>
 		</div>
 	);
 };
