913d6431fc1fbfab84830536e0bbae742393de0b
Jerome <cheonjh4572@gmail.com>
2022-03-16 13:29:15 +0900

    [FE_React] Update TaskList file to consider data requirements and props

--- FE_react/ts-webpack-app/src/components/TaskList.tsx
+++ FE_react/ts-webpack-app/src/components/TaskList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Task from './Task';
+import PropTypes from 'prop-types';
 
 const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
 	const events = {
@@ -57,3 +58,17 @@ const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }) => {
 };
 
 export default TaskList;
+TaskList.propTypes = {
+	/** Checks if it's in loading state */
+	loading: PropTypes.bool,
+	/** The list of tasks */
+	tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+	/** Event to change the task to pinned */
+	onPinTask: PropTypes.func,
+	/** Event to change the task to archived */
+	onArchiveTask: PropTypes.func,
+};
+
+TaskList.defaultProps = {
+	loading: false,
+};
