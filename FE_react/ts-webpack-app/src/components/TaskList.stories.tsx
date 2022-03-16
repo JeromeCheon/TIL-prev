d7c98be1a344b408f784629fcd55ccaaf82000a8
Jerome <cheonjh4572@gmail.com>
2022-03-16 12:49:01 +0900

    [FE_React] Add TaskList and its story component files as more complicated example

---
+++ FE_react/ts-webpack-app/src/components/TaskList.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import TaskList from './TaskList';
+import * as TaskStories from './Task.stories';
+
+export default {
+	component: TaskList,
+	title: 'TaskList',
+	// 데코레이터는 스토리에 임의의 wrapper를 제공하는 한 방법
+	// 여기서는 key를 사용, 기본 내보내기에서 렌더링 된 컴포넌트에 padding을 추가하는식으로 사용된다
+	// 또 데코레이터는 providers에서 스토리를 감싸줄 때 사용될 수 있다
+	decorators: [(story) => <div style={{ padding: '3rem' }}>{story()}</div>],
+};
+
+const Template = (args) => <TaskList {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	// Shaping the stories through args composition.
+	// The data was inherited from the Default story in task.stories.js.
+	tasks: [
+		{ ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+		{ ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+		{ ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+		{ ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+		{ ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+		{ ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+	],
+};
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+	// Shaping the stories through args composition.
+	// Inherited data coming from the Default story.
+	tasks: [
+		...Default.args.tasks.slice(0, 5),
+		{ id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+	],
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+	tasks: [],
+	loading: true,
+};
+
+export const Empty = Template.bind({});
+Empty.args = {
+	// Shaping the stories through args composition.
+	// Inherited data coming from the Loading story.
+	...Loading.args,
+	loading: false,
+};
