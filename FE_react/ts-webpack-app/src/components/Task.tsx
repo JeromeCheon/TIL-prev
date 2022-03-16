3810097e119c5f4abaca5c5dc53f47a494e28eb4
Jerome <cheonjh4572@gmail.com>
2022-03-16 09:06:51 +0900

    [FE_React] Update Task.tsx to use propTypes to prevent errors

--- FE_react/ts-webpack-app/src/components/Task.tsx
+++ FE_react/ts-webpack-app/src/components/Task.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Task = ({
 	task: { id, title, state },
@@ -35,4 +36,19 @@ const Task = ({
 	);
 };
 
+Task.propTypes = {
+	/** Composition of the task */
+	task: PropTypes.shape({
+		/** Id of the task */
+		id: PropTypes.string.isRequired,
+		/** Title of the task */
+		title: PropTypes.string.isRequired,
+		/** Current state of the task */
+		state: PropTypes.string.isRequired,
+	}),
+	/** Event to change the task to archived */
+	onArchiveTask: PropTypes.func,
+	/** Event to change the task to pinned */
+	onPinTask: PropTypes.func,
+};
 export default Task;
