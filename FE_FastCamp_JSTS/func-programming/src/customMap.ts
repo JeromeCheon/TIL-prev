6c65685414b607ffcb11ac13bb62af13a82889b8
Dev. Jerome <cheonjh4572@gmail.com>
2022-03-22 00:18:11 +0900

    [FE_TS] Update customMap function with more generic like map compose functions

--- FE_FastCamp_JSTS/func-programming/src/customMap.ts
+++ FE_FastCamp_JSTS/func-programming/src/customMap.ts
@@ -12,3 +12,15 @@ export const map = <A, B>(array: Array<A>, f: (a: A) => B): Array<B> => {
   }
   return result;
 };
+
+export type MapType<A, B> = (xs: Array<A>, f: (x: A) => B) => Array<B>;
+// (Array<A>, A => B) => Array<B>
+
+export type MapType1 = MapType<number, number>;
+// (Array<number>, number => number) => Array<number>
+
+export type Compose<A, B, C> = (g: (y: B) => C, f: (x: A) => B) => (a: A) => C;
+// (B=>C, A=>B) => A => C
+
+export type Compose1 = Compose<string, number, boolean>;
+// (number => boolean, string => number) => string => boolean
