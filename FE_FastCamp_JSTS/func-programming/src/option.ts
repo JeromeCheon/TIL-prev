3a56e853792b6a7730936e5e501cd78859dfe27d
Dev. Jerome <cheonjh4572@gmail.com>
2022-04-17 23:36:10 +0900

    [FE_TS] Update option file by adding custom map function for Option

--- FE_FastCamp_JSTS/func-programming/src/option.ts
+++ FE_FastCamp_JSTS/func-programming/src/option.ts
@@ -47,3 +47,11 @@ export const getOrElse = <A>(oa: Option<A>, defaultValue: A): A => {
   // 값이 있다면 해당 값을 사용한다.
   return oa.value;
 };
+
+// Option의 map 구현하기
+export const map = <A, B>(oa: Option<A>, f: (a: A) => B): Option<B> => {
+  // 값이 없으면 값이 없는 상태를 유지함
+  if (isNone(oa)) return oa;
+  // 값이 있으면 값을 함수에 적용함
+  return some(f(oa.value));
+};
