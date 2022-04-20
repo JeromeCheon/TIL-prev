36a4c35a28889f5b610f7b60a459a3383eefb69f
Jerome <cheonjh4572@gmail.com>
2022-04-20 13:44:31 +0900

    [BE_express-TDD] Update controller test for unit test error handling

--- BE_express/todo-tdd/tests/unit/todo.controller.test.js
+++ BE_express/todo-tdd/tests/unit/todo.controller.test.js
@@ -3,13 +3,13 @@ const TodoModel = require('../../model/todo.model');
 const httpMocks = require('node-mocks-http');
 const newTodo = require('../mock-data/new-todo.json');
 
-TodoModel.create = jest.fn();
+TodoModel.create = jest.fn(); // mock
 
 let req, res, next;
 beforeEach(() => {
 	req = httpMocks.createRequest();
 	res = httpMocks.createResponse();
-	next = null;
+	next = jest.fn();
 });
 
 describe('TodoController.createTodo', () => {
@@ -33,5 +33,12 @@ describe('TodoController.createTodo', () => {
 		TodoModel.create.mockReturnValue(newTodo);
 		await TodoController.createTodo(req, res, next);
 		expect(res._getJSONData()).toStrictEqual(newTodo);
+	});
+	it('should handle errors', async () => {
+		const errorMessage = { message: 'Done property missing' };
+		const rejectedPromise = Promise.reject(errorMessage);
+		TodoModel.create.mockReturnValue(rejectedPromise);
+		await TodoController.createTodo(req, res, next);
+		expect(next).toBeCalledWith(errorMessage);
 	});
 });
