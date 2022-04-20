0090d51449161fb1c72482507837f6a055e121a8
Jerome <cheonjh4572@gmail.com>
2022-04-20 12:47:52 +0900

    [BE_express-TDD] Update with mongoDB real db connection and let its be passed unit & int-test

--- BE_express/todo-tdd/app.js
+++ BE_express/todo-tdd/app.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const todoRoutes = require('./routes/todo.routes')
-const app = express()
+const express = require('express');
+const todoRoutes = require('./routes/todo.routes');
+const app = express();
+const mongodb = require('./mongodb/mongodb.connect');
 
-app.use('/todos', todoRoutes) // CRUD에 사용될 것이다 라는 의미
+mongodb.connect();
 
+app.use(express.json()); // integration test 에서 필요한 로직
+
+app.use('/todos', todoRoutes); // CRUD에 사용될 것이다 라는 의미
 app.get('/', (req, res) => {
-	res.send('Hello world~!')
-})
+	res.send('Hello world~!');
+});
 
-app.listen(3000, () => {
-	console.log('Server is now running!')
-})
+// app.listen(3000, () => {
+// 	console.log('Server is now running!');
+// });
 
-module.exports = app
+module.exports = app;
