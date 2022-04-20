311f6aa6f70098b6475a7d0802dd51ebfe46a6d1
Jerome <cheonjh4572@gmail.com>
2022-04-20 18:59:39 +0900

    [BE_express-TDD] Add updateTodo 404 test in unit test and logic

--- BE_express/todo-tdd/controllers/todo.controller.ts
+++ BE_express/todo-tdd/controllers/todo.controller.ts
@@ -40,7 +40,11 @@ exports.updateTodo = async (req, res, next) => {
 				useFindAndModify: false,
 			}
 		);
-		res.status(200).json(updatedTodo);
+		if (updatedTodo) {
+			res.status(200).json(updatedTodo);
+		} else {
+			res.status(404).json(updatedTodo);
+		}
 	} catch (err) {
 		next(err);
 	}
--- BE_express/todo-tdd/tests/unit/todo.controller.test.js
+++ BE_express/todo-tdd/tests/unit/todo.controller.test.js
@@ -47,6 +47,12 @@ describe('TodoController.updateTodo', () => {
 		await TodoController.updateTodo(req, res, next);
 		expect(next).toHaveBeenCalledWith(errorMessage);
 	});
+	it('should return 404 when item doesn t exist', async () => {
+		TodoModel.findByIdAndUpdate.mockReturnValue(null);
+		await TodoController.updateTodo(req, res, next);
+		expect(res.statusCode).toBe(404);
+		expect(res._isEndCalled()).toBeTruthy();
+	});
 });
 
 describe('TodoController.getTodoById', () => {
