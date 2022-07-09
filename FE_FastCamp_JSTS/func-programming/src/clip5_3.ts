f39933b8189ed0e0c8402c65ca2af577d08fb22b
Jerome <cheonjh4572@gmail.com>
2022-07-09 17:07:02 +0900

    Add clip5_3 to think of partial application

---
+++ FE_FastCamp_JSTS/func-programming/src/clip5_3.ts
@@ -0,0 +1,13 @@
+const delivery = (present: string, from: string, to: string) => {
+	return `
+  보내는 물건: ${present}
+  보내는 사람: ${from}
+  받는 사람: ${to}
+  `;
+};
+
+export const main = () => {
+	console.clear();
+
+	console.log(delivery('상품권', '엄마', '아들'));
+};
