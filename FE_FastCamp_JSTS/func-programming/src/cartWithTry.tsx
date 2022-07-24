580472daca42cad9cf817767eb2663a54f742356
Jerome <cheonjh4572@gmail.com>
2022-07-24 19:41:23 +0900

    Add KeepSuccess method in try.ts and accept it into cartWithTry

--- FE_FastCamp_JSTS/func-programming/src/cartWithTry.tsx
+++ FE_FastCamp_JSTS/func-programming/src/cartWithTry.tsx
@@ -1,42 +1,42 @@
 /** 기존 cart와 달리 구매하는 개수에 제한이 있는 경우를 구현할 것
  * 상품당 최대 수량을 10개로 제한하고, 개수가 1개 미만일 떄 에러를 발생하도록 구현하자
  */
-import { cart, Item } from "./cart";
-import React, { useEffect } from "react";
-import * as O from "./option";
-import * as T from "./try";
+import { cart, Item } from './cart';
+import React, { useEffect } from 'react';
+import * as O from './option';
+import * as T from './try';
 
-type ParsedItem = { _tag: "parsedItem" } & Item; // Item 타입을 확장한 형태로 만들었음
+type ParsedItem = { _tag: 'parsedItem' } & Item; // Item 타입을 확장한 형태로 만들었음
 type ParsedError = {
-  name: string;
-  message: string;
+	name: string;
+	message: string;
 };
 
 // 상품의 개수가 잘못되면 예외를 발생시키는 함수 구현해보자
 // 그리고 validateItem 을 Try를 반환하는 parseItem으로 바꿔보자
 const parseItem = (item: Item): T.Try<ParsedItem, ParsedError> => {
-  if (item.quantity < 1) {
-    return T.failed({
-      name: item.name,
-      message: "상품은 반드시 한개 이상 담아야 합니다.",
-    });
-  } else if (item.quantity > 10) {
-    return T.failed({
-      name: item.name,
-      message: "한 번에 10개를 초과하여 구매할 수 없습니다.",
-    });
-  }
-  // 검증이 완료되었으면 Item type의 값을 ParsedItem 타입의 값으로 변환하여 try 타입으로 리턴해야 함
-  return T.success({
-    _tag: "parsedItem",
-    ...item,
-  });
+	if (item.quantity < 1) {
+		return T.failed({
+			name: item.name,
+			message: '상품은 반드시 한개 이상 담아야 합니다.',
+		});
+	} else if (item.quantity > 10) {
+		return T.failed({
+			name: item.name,
+			message: '한 번에 10개를 초과하여 구매할 수 없습니다.',
+		});
+	}
+	// 검증이 완료되었으면 Item type의 값을 ParsedItem 타입의 값으로 변환하여 try 타입으로 리턴해야 함
+	return T.success({
+		_tag: 'parsedItem',
+		...item,
+	});
 };
-
-type ArrayItem = Array<Item>;
+// Item 에 대한 Array를 Parsed Item에 대한 Array로 바꿔보자.(ch6-8)
+export type ArrayItem = Array<T.Try<ParsedItem, ParsedError>>;
 
 const stockItem = (item: ParsedItem): string => {
-  return `
+	return `
   <li>
   <h2>${item.name}</h2>
   <div>가격: ${item.price}원</div>
@@ -60,90 +60,96 @@ const outOfStockItem = (item: ParsedItem): string => `
 `;
 
 const renderItem = (item: Item): string => {
-  const parsedItem = parseItem(item);
-  const render = T.map(parsedItem, (item) => {
-    if (item.outOfStock) {
-      // 현재 여기에 에러가 나. 왜? Try 내부 성공의 아이템 값을 반환하지 못하기 때문.
-      // 이를 해결하기 위해 Try의 map 함수가 사용된다.
-      // Try에서의 map은 실패했을 때는 자신 리턴, 성공일 때는 인자로 전달된 데이터를 자신에게 적용해서 그 결과를 리턴
-      // 이 과정에서 결과는 바뀌지 않아.
-      // 자 이제 try.ts에 map을 구현하자.
-      return outOfStockItem(item);
-      // return outOfStockItem(parsedItem);
-    } else {
-      return stockItem(item);
-    }
-  });
-  return T.getOrElse(render, errorItem);
+	const parsedItem = parseItem(item);
+	const render = T.map(parsedItem, (item) => {
+		if (item.outOfStock) {
+			// 현재 여기에 에러가 나. 왜? Try 내부 성공의 아이템 값을 반환하지 못하기 때문.
+			// 이를 해결하기 위해 Try의 map 함수가 사용된다.
+			// Try에서의 map은 실패했을 때는 자신 리턴, 성공일 때는 인자로 전달된 데이터를 자신에게 적용해서 그 결과를 리턴
+			// 이 과정에서 결과는 바뀌지 않아.
+			// 자 이제 try.ts에 map을 구현하자.
+			return outOfStockItem(item);
+			// return outOfStockItem(parsedItem);
+		} else {
+			return stockItem(item);
+		}
+	});
+	return T.getOrElse(render, errorItem);
 };
 
 const totalCalculator = (
-  list: Array<Item>,
-  getValue: (item: Item) => number
+	list: ArrayItem,
+	getValue: (item: ParsedItem) => number
 ): number => {
-  return (
-    list
-      // 1. 재고가 있는 상품만 분류하기
-      .filter((item) => {
-        try {
-          parseItem(item);
-          return item.outOfStock === false;
-        } catch (e) {
-          return false;
-        }
-      })
-      // 2. 분류된 상품들에 대해서 getValue 실행하기
-      .map(getValue)
-      // 3. getValue가 실행된 값 모두 더하기
-      .reduce((total, value) => total + value, 0)
-  );
+	return (
+		T.KeepSuccess(list)
+			// 1. 재고가 있는 상품만 분류하기
+			// Array<T.Try<ParseError, ParsedItem>> => Array<ParsedItem> -> try.ts에서 만들겠다.
+			.filter((item) => {
+				try {
+					// parseItem(item);
+					return item.outOfStock === false;
+				} catch (e) {
+					return false;
+				}
+			})
+			// 2. 분류된 상품들에 대해서 getValue 실행하기
+			.map(getValue)
+			// 3. getValue가 실행된 값 모두 더하기
+			.reduce((total, value) => total + value, 0)
+	);
 };
 
-const totalCount = (list: Array<Item>): string => {
-  const totalCount = totalCalculator(list, (item) => item.quantity);
-  return `<h2>전체 수량: ${totalCount}상자</h2>`;
+const totalCount = (list: ArrayItem): string => {
+	const totalCount = totalCalculator(list, (item) => item.quantity);
+	return `<h2>전체 수량: ${totalCount}상자</h2>`;
 };
 
-const totalPrice = (list: Array<Item>): string => {
-  const totalPrice = totalCalculator(
-    list,
-    (item) => item.price * item.quantity
-  );
+const totalPrice = (list: ArrayItem): string => {
+	const totalPrice = totalCalculator(
+		list,
+		(item) => item.price * item.quantity
+	);
 
-  const totalDiscountPrice = totalCalculator(list, (item) => {
-    // JS에서 파이프라이닝이 채택된다면 이렇게도 작성해볼 수 있을 것
-    // item.discountPrice |> O.fromUndefined($) \> O.getOrElse($, 0);
-    const discountPrice = O.getOrElse(O.fromUndefined(item.discountPrice), 0);
-    return discountPrice * item.quantity;
-  });
-  return `<h2>전체 가격: ${
-    totalPrice - totalDiscountPrice
-  }원 (총 ${totalDiscountPrice} 할인)</h2>`;
+	const totalDiscountPrice = totalCalculator(list, (item) => {
+		// JS에서 파이프라이닝이 채택된다면 이렇게도 작성해볼 수 있을 것
+		// item.discountPrice |> O.fromUndefined($) \> O.getOrElse($, 0);
+		const discountPrice = O.getOrElse(O.fromUndefined(item.discountPrice), 0);
+		return discountPrice * item.quantity;
+	});
+	return `<h2>전체 가격: ${
+		totalPrice - totalDiscountPrice
+	}원 (총 ${totalDiscountPrice} 할인)</h2>`;
 };
 
-export const CartList = (cartList: Array<Item>) => {
-  // CartList도 고차함수들로 refactoring 해보자.
+export const CartList = (cartList: ArrayItem) => {
+	// CartList도 고차함수들로 refactoring 해보자.
 
-  return `${cartList
-    // 1. 목록에 있는 아이템을 태그로 변경
-    .map(renderItem)
-    // 2. 태그의 목록을 모두 하나의 문자열로 연결
-    .reduce((tags, tag) => tags + tag, "")}`;
+	return `${cartList
+		// 1. 목록에 있는 아이템을 태그로 변경
+		.map((item) =>
+			T.getOrElse(
+				T.map(item, (parsedItem) => renderItem(parsedItem)),
+				errorItem
+			)
+		) // 명시적으로 바꿔보자
+		// 2. 태그의 목록을 모두 하나의 문자열로 연결
+		.reduce((tags, tag) => tags + tag, '')}`;
 };
 
 const PrintCartWithTryCatch: React.FC = () => {
-  useEffect(() => {
-    const mainBody = document.getElementById("main-body");
-    if (mainBody != null) {
-      const info = CartList(cart);
-      mainBody.innerHTML = `<h1>장바구니</h1>
+	useEffect(() => {
+		const mainBody = document.getElementById('main-body');
+		if (mainBody != null) {
+			const info = CartList(cart);
+			mainBody.innerHTML = `<h1>장바구니</h1>
       <ul>${info}</ul>
       ${totalCount(cart)}
       ${totalPrice(cart)}
       `;
-    }
-  });
-  return <></>;
+		}
+	});
+	return <></>;
 };
 
 export default PrintCartWithTryCatch;
--- FE_FastCamp_JSTS/func-programming/src/try.ts
+++ FE_FastCamp_JSTS/func-programming/src/try.ts
@@ -3,47 +3,70 @@
 // Option에서 some과 none으로 처리했던 것 처럼 try에서도 success/failed 처리하게.
 
 type Success<R> = {
-  readonly _tag: "success"; // 불변성 -> 참조 투명성 연관
-  readonly result: R;
+	readonly _tag: 'success'; // 불변성 -> 참조 투명성 연관
+	readonly result: R;
 };
 
 type Failed<E> = {
-  readonly _tag: "failed";
-  readonly error: E;
+	readonly _tag: 'failed';
+	readonly error: E;
 };
 
 export type Try<R, E> = Success<R> | Failed<E>;
 
 export const success = <R>(result: R): Try<R, never> => ({
-  _tag: "success",
-  result,
+	_tag: 'success',
+	result,
 });
 
 export const failed = <E>(error: E): Try<never, E> => ({
-  _tag: "failed",
-  error,
+	_tag: 'failed',
+	error,
 });
 
 export const isSuccess = <R>(ta: Try<R, unknown>): ta is Success<R> =>
-  ta._tag === "success";
+	ta._tag === 'success';
 
 export const isFailed = <E>(ta: Try<unknown, E>): ta is Failed<E> =>
-  ta._tag === "failed";
+	ta._tag === 'failed';
 
 export const getOrElse = <R, E>(
-  ta: Try<R, E>,
-  defaultValue: (e: E) => R
+	ta: Try<R, E>,
+	defaultValue: (e: E) => R
 ): R => {
-  // 에러가 있을 경우 기본 값을 사용
-  if (isFailed(ta)) return defaultValue(ta.error);
-  // 결과가 성공이라면 해당 값을 사용
-  return ta.result;
+	// 에러가 있을 경우 기본 값을 사용
+	if (isFailed(ta)) return defaultValue(ta.error);
+	// 결과가 성공이라면 해당 값을 사용
+	return ta.result;
 };
 
 // try의 map은 Option과 크게 다르지 않으나, 한가지 주의해야 할 점은
 // 성공했을 때 결과의 타입은 인자로 주어진 함수에 의해 변경될 수 있지만
 // 에러의 타입은 변경되지 않는다.
 export const map = <E, A, B>(ta: Try<A, E>, f: (a: A) => B): Try<B, E> => {
-  if (isFailed(ta)) return ta;
-  return success(f(ta.result));
+	if (isFailed(ta)) return ta;
+	return success(f(ta.result));
 };
+
+// Array<T.Try<ParseError, ParsedItem>> => Array<ParsedItem>
+// export const KeepSuccess = <E, R>(tas: Array<Try<R, E>>): Array<R> => {
+//   const ret = tas.map((ta) => {
+//     if (isSuccess(ta)) return ta.result;
+//     else return ta.error
+//     // 근데 반환값이 성공값이기 때문에 error를 리턴해도 undefined를 함축한다.
+//     // 그러므로 단순히 map함수만으로는 역부족이야.
+//     // 이 문제를 해소하는 것이 flatMap
+//   })
+//   return ret;
+// };
+
+// flatMap :: (A => Array<B>) => (Array<A> => Array<B>)
+// map :: (A => B)   => (Array<A> => Array<B>)
+export const KeepSuccess = <E, R>(tas: Array<Try<R, E>>): Array<R> => {
+	const ret = tas.flatMap((ta) => {
+		if (isSuccess(ta)) return [ta.result];
+		else return [];
+	});
+	return ret;
+};
+// 이제 우리가 만든 KeepSuccess를 totalCalculator에 적용해볼 차례.
