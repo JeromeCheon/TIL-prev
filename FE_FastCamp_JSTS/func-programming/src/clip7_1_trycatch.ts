f35e39d8d5aababfa386663d9b5056fffd2e0431
Jerome <cheonjh4572@gmail.com>
2022-07-31 23:48:55 +0900

    Add trycatch example to review before learning async

---
+++ FE_FastCamp_JSTS/func-programming/src/clip7_1_trycatch.ts
@@ -0,0 +1,44 @@
+const f = (a: string) => {
+	if (a === '') {
+		throw new Error('빈 문자열은 입력할 수 없습니다.');
+	}
+	return a.length * 2;
+};
+
+const g = (n: number) => {
+	if (n === 6) {
+		throw new Error('6은 입력할 수 없습니다.');
+	}
+	return n + 1;
+};
+
+const h = (x: number) => {
+	if (x === 5) {
+		throw Error('5는 입력할 수 없습니다.');
+	}
+	return x % 3 === 0;
+};
+
+const handleError = (e: unknown) => {
+	// 사용자에게 에러를 알려주는 통합 함수
+	console.log('handleError: ' + e);
+};
+
+const greeting = (name: string) => {
+	console.log('Hello, ', name);
+};
+
+const program = (b: boolean) => {
+	console.log(b);
+};
+export const reviewEHandling = () => {
+	const a = 'test';
+	const b = f(a);
+	const c = g(b);
+	const d = h(c);
+
+	program(d);
+
+	greeting('world');
+	console.log('프로그램이 종료되었습니다.');
+};
