d85ceb366d0b27048ceb40c67b8f17a24f436439
Jerome <cheonjh4572@gmail.com>
2022-08-06 17:33:19 +0900

    Add Promise intro file by using callback style

---
+++ FE_FastCamp_JSTS/func-programming/src/clip7_5.ts
@@ -0,0 +1,34 @@
+// Promise 소개라는 내용으로 전개
+// Option<A> = None | Some<A>
+// Try<E, A> = Failed<E> | Success<A>
+// CPS는 비동기라고 명명하겠다.
+// Async<A> = ???
+
+// 함수 f, g, h 는 모두 인자를 두개씩 갖고 있는
+// 첫번째 인자는 함수계산에 사용되는 값
+// 두 번째 인자는 계산이 끝나면 결과를 전달할 콜백함수
+// 일단 아래 형태는 커링을 사용해서 함수의 함수형태로 만들 수 있다
+const f = (str: string, ret: (x: number) => void): void => {
+	setTimeout(() => {
+		console.log('f 호출: ' + str);
+		ret(str.length * 2);
+	}, 500);
+};
+
+const g = (n: number, ret: (x: number) => void): void => {
+	setTimeout(() => {
+		console.log('g 호출: ' + n);
+		ret(n + 1);
+	}, 500);
+};
+
+const h = (x: number, ret: (x: boolean) => void): void => {
+	setTimeout(() => {
+		console.log('h 호출: ' + x);
+		ret(x % 3 === 0);
+	}, 500);
+};
+
+const handleError = (e: unknown) => {
+	// 사용자에게 에러를 알려주는 통합 함수
+};
