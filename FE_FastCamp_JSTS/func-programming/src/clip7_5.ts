47bde1be4fc7a3edda394765abf5fb7646750d3a
Jerome <cheonjh4572@gmail.com>
2022-08-09 23:57:46 +0900

    Update comment about async-await preview

--- FE_FastCamp_JSTS/func-programming/src/clip7_5.ts
+++ FE_FastCamp_JSTS/func-programming/src/clip7_5.ts
@@ -7,34 +7,34 @@
 type Async<A> = (ret: (x: A) => void) => void;
 
 const resolve = <A>(a: A): Async<A> => {
-  return (ret) => {
-    ret(a);
-  };
+	return (ret) => {
+		ret(a);
+	};
 };
 
 // 그렇다면 Async 에도 map과 flatMap이 있을까? 있긴 한데 그 형태가 좀 달라
 // flatMap 부터 먼저 만들어보자
 
 const flatMap = <A, B>(a: Async<A>, f: (a: A) => Async<B>): Async<B> => {
-  return (ret) => {
-    a((a_) => {
-      const b = f(a_);
-      b((b_) => ret(b_));
-    });
-  };
+	return (ret) => {
+		a((a_) => {
+			const b = f(a_);
+			b((b_) => ret(b_));
+		});
+	};
 };
 
 // 이제 flatMap을 사용해서 map을 구현해보자.
 const map = <A, B>(a: Async<A>, f: (a: A) => B): Async<B> => {
-  return flatMap(a, (a_) => resolve(f(a_)));
-  // return flatMap(a, (a_) => f(a_)); // 이렇게 하면 에러가 나는데 이유는 이 결과값인 B를 Async<B>에 할당할 수 없기 때문.
-  // Option과 Try에서 해준 역할 비슷한걸 resolve라 명명하고 만들어보자.
+	return flatMap(a, (a_) => resolve(f(a_)));
+	// return flatMap(a, (a_) => f(a_)); // 이렇게 하면 에러가 나는데 이유는 이 결과값인 B를 Async<B>에 할당할 수 없기 때문.
+	// Option과 Try에서 해준 역할 비슷한걸 resolve라 명명하고 만들어보자.
 };
 
 const run = <A>(a: Async<A>) => {
-  a(() => {
-    return;
-  });
+	a(() => {
+		return;
+	});
 };
 
 // clip 7-5,6
@@ -49,56 +49,56 @@ const run = <A>(a: Async<A>) => {
 // 이전 Async 와 CPS 적인 측면에서 크게 벗어나지 않다.
 // 값을 반환할 때는 ret를 사용했지만, promise는 관례적으로 resolve를 사용함 + 두번째 인자 reject
 const f = (str: string): Promise<number> =>
-  new Promise((resolve, reject) => {
-    if (str === "") {
-      reject("빈 문자열은 입력할 수 없습니다.");
-      return;
-    }
-    setTimeout(() => {
-      console.log("f 호출: " + str);
-      resolve(str.length * 2);
-    }, 500);
-  });
+	new Promise((resolve, reject) => {
+		if (str === '') {
+			reject('빈 문자열은 입력할 수 없습니다.');
+			return;
+		}
+		setTimeout(() => {
+			console.log('f 호출: ' + str);
+			resolve(str.length * 2);
+		}, 500);
+	});
 
 const g = (n: number): Promise<number> =>
-  new Promise((resolve, reject) => {
-    if (n === 6) {
-      reject("6은 입력할 수 없습니다.");
-      return;
-    }
-    setTimeout(() => {
-      console.log("g 호출: " + n);
-      resolve(n + 1);
-    }, 500);
-  });
+	new Promise((resolve, reject) => {
+		if (n === 6) {
+			reject('6은 입력할 수 없습니다.');
+			return;
+		}
+		setTimeout(() => {
+			console.log('g 호출: ' + n);
+			resolve(n + 1);
+		}, 500);
+	});
 
 const h = (x: number): Promise<boolean> =>
-  new Promise((resolve, reject) => {
-    if (x === 5) {
-      reject("5는 입력할 수 없습니다.");
-      return;
-    }
-    setTimeout(() => {
-      console.log("h 호출: " + x);
-      resolve(x % 3 === 0);
-    }, 500);
-  });
+	new Promise((resolve, reject) => {
+		if (x === 5) {
+			reject('5는 입력할 수 없습니다.');
+			return;
+		}
+		setTimeout(() => {
+			console.log('h 호출: ' + x);
+			resolve(x % 3 === 0);
+		}, 500);
+	});
 
 const handleError = (e: unknown) => {
-  // 사용자에게 에러를 알려주는 통합 함수
-  console.log("handleError: " + e);
+	// 사용자에게 에러를 알려주는 통합 함수
+	console.log('handleError: ' + e);
 };
 
 const program = (s: boolean) => {
-  console.log(s);
+	console.log(s);
 };
 
 const greeting = (name: string) => {
-  console.log("Hello, " + name);
+	console.log('Hello, ' + name);
 };
 
 export const promMain1 = () => {
-  /*
+	/*
 	const a = f('test');
 	const b = flatMap(a, (a_) => g(a_));
 	const c = flatMap(b, (b_) => h(b_));
@@ -107,7 +107,7 @@ export const promMain1 = () => {
 	// 이 값을 리턴해주는 함수를 run이라고 명명하고 구현해보자
 	run(result);
   */
-  /*
+	/*
 	const a = f('test');
 	// 이제 g함수에 a를 적용하려면? a가 Promise이니 바로 넣을 수는 없고 flatMap과 비슷한 함수가 필요해
 	// Promise에서는 then 함수가 그 역할을 대신함
@@ -117,14 +117,14 @@ export const promMain1 = () => {
 	const result = c.then((c_) => program(c_));
   result.catch(handleError); // catch 사용은 Try의 getOrElse 사용을 연상케해
   */
-  // 그리고 중간 변수 사용없이 바로 chaining도 가능해
-  f("abc")
-    // .then((a_) => g(a_)) 또 then에 사용된 함수는 인자를 직접 넘겨주기 때문에 아래와 같이 줄일 수도 있어
-    .then(g)
-    .then(h)
-    .then(program)
-    .catch(handleError);
-  // 이제 async-await을 배워보자
-  greeting("world");
-  console.log("프로그램 종료");
+	// 그리고 중간 변수 사용없이 바로 chaining도 가능해
+	f('abc')
+		// .then((a_) => g(a_)) 또 then에 사용된 함수는 인자를 직접 넘겨주기 때문에 아래와 같이 줄일 수도 있어
+		.then(g)
+		.then(h)
+		.then(program)
+		.catch(handleError);
+	// 이제 async-await을 배워보자 -> 비동기적 실행을 명령적인 방식으로 실행하는 것
+	greeting('world');
+	console.log('프로그램 종료');
 };
