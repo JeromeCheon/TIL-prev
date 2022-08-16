364e410a127f16a13b16af24d7dc8bc5f010a7aa
Jerome <cheonjh4572@gmail.com>
2022-08-16 18:01:18 +0900

    Add lift function and liftedMap and filter using currying concept

--- FE_FastCamp_JSTS/func-programming/src/clip8_4.ts
+++ FE_FastCamp_JSTS/func-programming/src/clip8_4.ts
@@ -33,4 +33,43 @@ const filter =
 		};
 	};
 
+const filterObserver =
+	<A>(pred: (a: A) => boolean) =>
+	(subscribe: Observer<A>): Observer<A> => {
+		// 출력은 a타입의 값을 입력받는 함수이고
+		return (a) => {
+			// 값이 조건에 맞을 때만 subscribe에 값을 전달함
+			if (pred(a)) {
+				subscribe(a);
+			}
+		};
+	};
+
+// map :: (A => B) => Array<A> => Array<B>
+// mapObserver :: (A => B) => Observer<B> => Observer<A>
+// 지금까지의 map 함수와는 달리 Observer<B>를 입력받고 Observer<A> 를 리턴해
+const mapObserver =
+	<A, B>(f: (a: A) => B) =>
+	(subscribe: Observer<B>): Observer<A> => {
+		return (a) => {
+			subscribe(f(a));
+		};
+	};
+
+// map :: (A => B) => Observable<A> => Observable<B>
+// lift :: (Observer<B> => Observer<A>) => Observable<A> => Observable<B>
+const lift =
+	<A, B>(f: (b: Observer<B>) => Observer<A>) =>
+	(source: Observable<A>): Observable<B> => {
+		// Observable은 subscribe가 전달되는 함수
+		return (subscribe) => {
+			// source에는 observer<A>가 전달되어야 함
+			source(f(subscribe));
+		};
+	};
+
+// const liftedMap = <A, B>(f: (a: A) => B) => lift(mapObserver(f));
+const liftedMap = pipeFunctions(mapObserver, lift);
+const liftedFilter = pipeFunctions(filterObserver, lift);
+// 함수들을 커링해서 만들었기 때문에 이러한 방식이 가능한 것
 export const observMain4 = () => {};
