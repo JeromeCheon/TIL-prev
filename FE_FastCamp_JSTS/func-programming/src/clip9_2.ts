5c1a1eaab8092258c8b321009d12b9c4fcc67470
Jerome <cheonjh4572@gmail.com>
2022-08-20 14:30:16 +0900

    Update with Function type and contraMap

--- FE_FastCamp_JSTS/func-programming/src/clip9_2.ts
+++ FE_FastCamp_JSTS/func-programming/src/clip9_2.ts
@@ -46,4 +46,38 @@ const optionGetLength = O.map(getLength);
 const of1 = O.map(compose(isEven)(getLength));
 
 const of2 = compose(O.map(isEven))(O.map(getLength));
+
+type Iterator<A> = () => A;
+type Observer<A> = (a: A) => void;
+type Function<A, B> = (a: A) => B;
+
+const map =
+	<A, B>(f: (a: A) => B) =>
+	<R>(input: Function<R, A>): Function<R, B> => {
+		return (r) => {
+			return f(input(r)); // 구조가 compose와 익숙하지?
+		};
+	};
+
+// mapIsEven :: (R => number) => (R => boolean)
+const mapIsEven = map(isEven);
+const c1 = mapIsEven(getLength);
+
+// 입력 타입에 대한 map은 contraMap이라고 함
+// map의 일종이기 때문에 함수를 인자로 받음
+const contraMap =
+	<A, B>(f: (a: A) => B) =>
+	<R>(input: Function<B, R>): Function<A, R> => {
+		// map을 구현했을 때와 반대로 인풋의 결과가 리턴되어야 하고
+		// input에 f(a)가 적용된 결과가 리턴되어야 함
+		return (a) => {
+			return input(f(a));
+		};
+	};
+// 출력타입이 무엇이든 입력이 number인 함수를 입력하면
+// 그것을 입력이 string인 함수로 변환시켜주는 함수
+// map이 출력 타입을 변환시키는데 함수인자가 사용되었다면
+// contraMap은 입력 타입을 변환시키는데에 사용
+const contraMapGetLength = contraMap(getLength);
+
 export const observMain5 = () => {};
