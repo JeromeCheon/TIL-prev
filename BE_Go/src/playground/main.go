84a78a63cabfad06a4849ddf02a3cb31eded0f15
Jerome <cheonjh4572@gmail.com>
2022-08-30 08:57:07 +0900

    Update go playground with switch practice

--- BE_Go/src/playground/main.go
+++ BE_Go/src/playground/main.go
@@ -1,21 +1,19 @@
 package main
 
-import "fmt"
-
-const (
-	apple = iota // 0
-	grape
-	Orange
-)
-
-
 func main () {
-	fmt.Println(apple, grape, Orange)
+	var name string
+	var category = 1
+
+	// Expression 사용했을 때
+	switch x := category << 2; x-1 {
+	case 1:
+		name = "Paper Book"
+	case 2:
+		name = "eBook"
+	case 3:
+		name = "Blog"
+	default:
+		name = "Other"
+	}
+	println(name)
 }
-/*
-go 프로그래밍 언어는 다음과 같은 25개의 예약 키워드를 갖는다.
-이들 Go 키워드들은 변수명, 상수명, 함수명 등의 identifier로 사용할 수 없음
-break, default, func, interface, select, case, defer, go, map, struct, chan
-else, goto, package, switch, const, fallthrough, if, range, type, continue, for
-import, return, var
-*/
