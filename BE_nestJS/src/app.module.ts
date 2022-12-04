07ef4c43aee622d913ec65257c768a09bc27645f
Jerome <cheonjh4572@gmail.com>
2022-12-04 23:34:02 +0900

    [BE_nestJS] Update app module with socketIO chatgateway

--- BE_nestJS/src/app.module.ts
+++ BE_nestJS/src/app.module.ts
@@ -1,9 +1,10 @@
 import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [],
+  providers: [ChatGateway],
 })
 export class AppModule {}
