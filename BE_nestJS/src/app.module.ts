2ec559b015e3aec4647a44cbafa14b39ed9fde28
Jerome <cheonjh4572@gmail.com>
2022-12-04 19:05:47 +0900

    Delete movies domain to reset new nest project

--- BE_nestJS/src/app.module.ts
+++ BE_nestJS/src/app.module.ts
@@ -1,9 +1,8 @@
 import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
-import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [],
   controllers: [AppController],
   providers: [],
 })
