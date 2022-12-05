eed6305207251f9edf64f13959e4539608f73217
Dev. Jerome <cheonjh4572@gmail.com>
2022-12-05 22:17:15 +0900

    [BE_nestJS] Update with nest common onModuleInit to study sockIO on method

--- BE_nestJS/src/chat.gateway.ts
+++ BE_nestJS/src/chat.gateway.ts
@@ -1,14 +1,23 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnModuleInit {
   @WebSocketServer()
-  server;
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log.apply(socket.id);
+      console.log('Connected');
+    });
+  }
 
   @SubscribeMessage('message')
   // handleMessage(client, data): void {} // client 직접적으로 사용하면 이렇게도 가능
