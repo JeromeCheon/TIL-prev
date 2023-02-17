01d7127598fae3975fe7ac2b69eb8c71cedd3c63
Jerome <cheonjh4572@gmail.com>
2023-02-17 15:35:53 +0900

    Delete existing go playgrounds and add basic tutorial file

--- BE_Go/src/basic/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main()  {
-	// function을 import할 때, name이 대문자이면 public, 소문자이면 private한 성격을 갖는다
-	// 따라서 어떤 경로의 package를 import 할 때 나타나는 옵션은 대문자로 정의된 함수밖에 안 나타난다
-	fmt.Println("Hello world with formatting package")
-}
