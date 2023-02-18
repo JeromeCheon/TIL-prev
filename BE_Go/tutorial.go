46af94098df3ffebbf8e749f586c0f4865bd848c
Jerome <cheonjh4572@gmail.com>
2023-02-18 23:44:45 +0900

    Update tutorial with type casting practice

--- BE_Go/tutorial.go
+++ BE_Go/tutorial.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 10
-	var msg string = "Variable tutorial"
-	a = 20
-	msg = "Change msg variable"
-	fmt.Println(msg, a)
+	var a int8 = 3
+	var b float32 = 16.223
+	var c = 4
+	d := 5 // 이 형태를 선언 대입문이라고 한다. 숫자는 int64로, 실수는 float64로 읽는다
+
+	fmt.Println(a, b, c, d)
+
+	// 타입변환
+	var e int = int(b)
+	f := float64(c * d)
+	fmt.Println(e, f)
 }
