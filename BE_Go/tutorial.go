37ec5a75a96addc08e9d07cd86295bc47cb8e339
Jerome <cheonjh4572@gmail.com>
2023-02-19 01:41:16 +0900

    Update tutorial with various operators

--- BE_Go/tutorial.go
+++ BE_Go/tutorial.go
@@ -1,37 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func equal(a, b float64) bool {
+	return math.Nextafter(a, b) == b // 값 비교
+}
 
 func main() {
-	var a int8 = 3
-	var b float32 = 16.223
-	var c = 4
-	d := 5 // 이 형태를 선언 대입문이라고 한다. 숫자는 int64로, 실수는 float64로 읽는다
+	var a float64 = 0.1
+	var b float64 = 0.2
+	var c float64 = 0.3
 
-	fmt.Println(a, b, c, d)
+	fmt.Printf("%0.18f + %0.18f = %0.18f\n", a, b, a+b)
+	fmt.Printf("%0.18f === %0.18f : %v\n", c, a+b, equal(a+b, c))
 
-	// 타입변환
-	var e int = int(b)
-	f := float64(c * d)
-	fmt.Println(e, f)
+	a = 0.000000004
+	b = 0.000000002
+	c = 0.000000007
 
-	// 표준출력: 특수문자 fmt Format 처리
-	str := "Hello\tGo\t\tWorld\n\"Go\" is Awesome!\n"
-	fmt.Print(str)
-	fmt.Printf("%s", str)
-	fmt.Printf("%q", str)
-
-	/* 표준입력: 
-	Scan() -> 표준 입력에서 값을 입력받음, 
-	Scanf() -> 표준 입력에서 서식 형태로 값을 입력받음 
-	Scanln() -> 표준 입력에서 한 줄 읽어서 값을 입력 받음 */
-	var scanA int
-	var scanB int
-
-	n, err := fmt.Scan(&scanA, &scanB)
-	if( err != nil) {
-		fmt.Println(n, err)
-	} else {
-		fmt.Println(n, scanA, scanB)
-	}
+	fmt.Printf("%g == %g : %v\n", c, a+b, equal(a+b, c))
 }
