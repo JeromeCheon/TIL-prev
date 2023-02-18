c41b5ab7b6f8fbe3670758d2a8cedddb54d22b76
Jerome <cheonjh4572@gmail.com>
2023-02-18 21:01:45 +0900

    Update tutorial with basic variable code

--- BE_Go/tutorial.go
+++ BE_Go/tutorial.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var number uint8 = 255
-	fmt.Println("Hello world~", number)
+	var a int = 10
+	var msg string = "Variable tutorial"
+	a = 20
+	msg = "Change msg variable"
+	fmt.Println(msg, a)
 }
