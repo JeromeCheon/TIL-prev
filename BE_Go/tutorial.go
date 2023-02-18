ae985f78f5b701965ecdac667b288c4a8800b5f9
Jerome <cheonjh4572@gmail.com>
2023-02-19 00:52:26 +0900

    Update tutorial with fmt Print & Scan

--- BE_Go/tutorial.go
+++ BE_Go/tutorial.go
@@ -14,4 +14,24 @@ func main() {
 	var e int = int(b)
 	f := float64(c * d)
 	fmt.Println(e, f)
+
+	// 표준출력: 특수문자 fmt Format 처리
+	str := "Hello\tGo\t\tWorld\n\"Go\" is Awesome!\n"
+	fmt.Print(str)
+	fmt.Printf("%s", str)
+	fmt.Printf("%q", str)
+
+	/* 표준입력: 
+	Scan() -> 표준 입력에서 값을 입력받음, 
+	Scanf() -> 표준 입력에서 서식 형태로 값을 입력받음 
+	Scanln() -> 표준 입력에서 한 줄 읽어서 값을 입력 받음 */
+	var scanA int
+	var scanB int
+
+	n, err := fmt.Scan(&scanA, &scanB)
+	if( err != nil) {
+		fmt.Println(n, err)
+	} else {
+		fmt.Println(n, scanA, scanB)
+	}
 }
