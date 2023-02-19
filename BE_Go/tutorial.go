3c2edb3035dd3a5e1ee9b29399888fedfbf02f6a
Jerome <cheonjh4572@gmail.com>
2023-02-20 01:02:42 +0900

    Update tutorial with for loop

--- BE_Go/tutorial.go
+++ BE_Go/tutorial.go
@@ -1,32 +1,46 @@
 package main
 
-import "fmt"
-
-
-func getMyAge() int {
-	return 22
-}
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	day := "thursday"
-	
-	switch day {
-	case "monday", "tuesday":
-		fmt.Println("월, 화요일은 수업 가는 날입니다.")
-	case "wednesday", "thursday", "friday":
-		fmt.Println("수, 목, 금요일은 실습 가는 날입니다.")
+	/*
+		for 문. Go에서는 반복문으로 for 하나만 지원하지만 여러형태가 있음을 알아야해
+		기본 형태는
+		for 초기문; 조건문; 후처리 {
+			code block	-> 조건문이 true인 경우 수행
+		}
+		1.1 초기문 생략. 생략해도 ;붙여서 조건문 자리를 표시해줘야 함
+		1.2 후처리 생략. 생략해도 조건문 뒤에 ;붙여줘야 함
+		1.3 조건문만 있을 때
+		for ; 조건문 ; {
+			code block
+		} or 
+		for 조건문 {
+			code block
+		} 이 때 조건문이 true이면 무한 루프가 된다. 
+		한편, 중첩 for문을 구성할 때 break 범위를 지정하고 싶다면 flag 변수를 사용하거나 레이블을 지정하면 된다
+	*/
+	stdin := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("입력하세요.")
+		var number int
+		_, err := fmt.Scanln(&number)
+		if err != nil {
+			fmt.Println("숫자를 입력하세요.")
+
+			// 키보드 버퍼를 비웁니다.
+			stdin.ReadString('\n')
+			continue
+		}
+		fmt.Printf("입력하신 숫자는 %d입니다.\n", number)
+		if number%2 == 0 {
+			break
+		}
 	}
-// switch 초기문
-// if 문과 마찬가지로 switch문에서도 초기문을 넣을 수 있음
-	switch age := getMyAge(); age {
-	case 10:
-		fmt.Println("Teen")
-	case 33:
-		fmt.Println("Pair 3")
-	default:
-		fmt.Println("My age is", age)
-	}
-	// 한편, Go 언어에서는 break를 사용하지 않아도 case 하나를 실행 후 자동으로 switch문을 빠져나가게 돼
-	// 하나의 case문 실행 후 다음 case까지 같이 실행하고 싶다면? fallthrough 키워드 사용!
-	// 그러나 코드를 보는 사람에게는 혼동을 일으킬 수 있으므로 사용하지 않는 것을 권장함
+	fmt.Println("for 문이 종료되었습니다.")
 }
