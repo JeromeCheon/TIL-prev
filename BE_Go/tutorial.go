39f258ec861d2617df015aa1ed6cb3e3ebf53824
Jerome <cheonjh4572@gmail.com>
2023-02-20 00:47:24 +0900

    Update tutorial with switch statement

--- BE_Go/tutorial.go
+++ BE_Go/tutorial.go
@@ -1,25 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-func equal(a, b float64) bool {
-	return math.Nextafter(a, b) == b // 값 비교
+
+func getMyAge() int {
+	return 22
 }
 
 func main() {
-	var a float64 = 0.1
-	var b float64 = 0.2
-	var c float64 = 0.3
-
-	fmt.Printf("%0.18f + %0.18f = %0.18f\n", a, b, a+b)
-	fmt.Printf("%0.18f === %0.18f : %v\n", c, a+b, equal(a+b, c))
-
-	a = 0.000000004
-	b = 0.000000002
-	c = 0.000000007
-
-	fmt.Printf("%g == %g : %v\n", c, a+b, equal(a+b, c))
+	day := "thursday"
+	
+	switch day {
+	case "monday", "tuesday":
+		fmt.Println("월, 화요일은 수업 가는 날입니다.")
+	case "wednesday", "thursday", "friday":
+		fmt.Println("수, 목, 금요일은 실습 가는 날입니다.")
+	}
+// switch 초기문
+// if 문과 마찬가지로 switch문에서도 초기문을 넣을 수 있음
+	switch age := getMyAge(); age {
+	case 10:
+		fmt.Println("Teen")
+	case 33:
+		fmt.Println("Pair 3")
+	default:
+		fmt.Println("My age is", age)
+	}
+	// 한편, Go 언어에서는 break를 사용하지 않아도 case 하나를 실행 후 자동으로 switch문을 빠져나가게 돼
+	// 하나의 case문 실행 후 다음 case까지 같이 실행하고 싶다면? fallthrough 키워드 사용!
+	// 그러나 코드를 보는 사람에게는 혼동을 일으킬 수 있으므로 사용하지 않는 것을 권장함
 }
