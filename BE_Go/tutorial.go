f43858b5179afea879db9b71c177e8fbbb0759c8
Jerome <cheonjh4572@gmail.com>
2023-02-20 14:39:33 +0900

    Update tutorial with pointer practice

--- BE_Go/tutorial.go
+++ BE_Go/tutorial.go
@@ -1,41 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
+import "fmt"
+
+/*
+포인터는 왜, 언제 쓰나?
+변수 대입이나 함수 인수 전달은 항상 값을 복사하기 때문에 많은 메모리 공간을 사용하는 문제와 큰 메모리 공간을 복사할 때 발생하는 성능 문제를 안고 있음
+또 다른 공간으로 복사되기 때문에 변경 사항이 적용되지도 않음.
+*/
 
 type User struct {
 	Name string
-	ID   string
-	Age  int
+	Age  int8
 }
 
-type VIPUser struct {
-	User
-	VIPLevel int32
-	Price    float64
+func NewUser(a string, b int8) *User {
+	var u = User{a, b}
+	return &u
 }
 
-/*
-한편, 구조체 변수가 선언되면 컴퓨터는 구조체 필드를 모두 담을 수 있는 메모리 공간을 할당함
-그리고 구조체 변숫값을 다른 구조체에 대입하면 모든 필드값이 복사됨
-*/
 func main() {
-	user := User{"천재홍", "jerome", 29}
-	vip := VIPUser{
-		User{"재홍", "jaehong", 28},
-		3,
-		250,
-	}
+	var a int = 500
+	var p *int
+	p = &a
 
-	fmt.Printf("유저: %s ID: %s age: %d\n", user.Name, user.ID, user.Age)
-	fmt.Printf("VIP 유저: %s ID: %s 나이: %d VIP 레벨: %d VIP 가격: %f만원\n",
-		vip.Name,
-		vip.ID,
-		vip.Age,
-		vip.VIPLevel,
-		vip.Price, // 여러 줄로 초기화 할 때는 제일 마지막 값 뒤에 꼭 쉼표를 달아주자
-	)
-	fmt.Println(unsafe.Sizeof(vip))
+	fmt.Printf("p의 값: %p\n", p)
+	fmt.Printf("p가 가리키는 메모리의 값: %d\n", *p)
+	*p = 100
+	fmt.Printf("a의 값: %d\n", a)
+
+	userPointer := NewUser("AAA", 23)
+
+	fmt.Print(userPointer)
 }
