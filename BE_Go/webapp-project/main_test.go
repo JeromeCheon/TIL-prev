5361380123467762cfbfbe41d6b51497376b6427
Jerome <cheonjh4572@gmail.com>
2023-02-24 12:02:35 +0900

    Add json data handler test code

--- BE_Go/webapp-project/main_test.go
+++ BE_Go/webapp-project/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -39,4 +40,23 @@ func TestBarHandler(t *testing.T) {
 	data, _ := io.ReadAll(res.Body)       // 데이터를 읽어서 확인
 	assert.Equal("Hello World! id:0", string(data))
 
+}
+
+func TestStudentHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/student", nil) // "/student" path test
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/student", web.StudentHandler)
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	student := new(web.Student)
+	err := json.NewDecoder(res.Body).Decode(student)
+	assert.Nil(err)
+	assert.Equal("aaa", student.Name)
+	assert.Equal(16, student.Age)
+	assert.Equal(87, student.Score)
 }
