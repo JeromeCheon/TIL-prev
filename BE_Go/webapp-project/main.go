e3e6e97d727ec7b40f74e39af4954f68c5f40e0d
Jerome <cheonjh4572@gmail.com>
2023-02-24 10:53:10 +0900

    Update http handler with newserveMux

--- BE_Go/webapp-project/main.go
+++ BE_Go/webapp-project/main.go
@@ -10,6 +10,8 @@ func main() {
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // 두번째 인자로 함수 리터럴 바로 줄때
 	// 	fmt.Fprint(w, "Hello world")
 	// })
-	http.HandleFunc("/bar", web.HandleRequest) // "/bar" 핸들러 등록
-	http.ListenAndServe(":3000", nil)          // 두번째 인자가 nil이면 DefaultServeMux를 사용한다는 것
+	mux := http.NewServeMux() // serveMux 인스턴스 생성
+	mux.HandleFunc("/", web.HandleRequest)
+	mux.HandleFunc("/bar", web.HandleRequest)
+	http.ListenAndServe(":3000", mux) // 두번째 인자가 nil이면 DefaultServeMux를 사용한다는 것
 }
