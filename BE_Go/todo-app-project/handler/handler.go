d1dd62b072aad227e127208cdd1efeae5fc9be72
Jerome <cheonjh4572@gmail.com>
2023-03-01 01:31:58 +0900

    Update handler file by implementing update, delete handler logic

--- BE_Go/todo-app-project/handler/handler.go
+++ BE_Go/todo-app-project/handler/handler.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 	"github.com/urfave/negroni"
+	"log"
 	"net/http"
 	"sort"
+	"strconv"
 )
 
 var Rd *render.Render
@@ -14,6 +17,10 @@ type Todo struct { // Todo 정보를 담는 구조체
 	ID        int    `json:"id,omitempty"` // JSON 포맷으로 변환 옵션
 	Name      string `json:"name"`
 	Completed bool   `json:"completed,omitempty"`
+}
+
+type Success struct {
+	Success bool `json:"success"`
 }
 
 var todoMap map[int]Todo
@@ -41,9 +48,47 @@ func GetTodoListHandler(w http.ResponseWriter, r *http.Request) {
 	Rd.JSON(w, http.StatusOK, list) // ID로 정렬해서 전체 목록 반환
 }
 
-func PostTodoHandler(w http.ResponseWriter, r *http.Request)   {}
-func UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {}
-func DeleteTodoHandler(w http.ResponseWriter, r *http.Request) {}
+func PostTodoHandler(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	lastID++
+	todo.ID = lastID
+	todoMap[lastID] = todo
+	Rd.JSON(w, http.StatusCreated, todo)
+}
+func UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
+	var newTodo Todo
+	err := json.NewDecoder(r.Body).Decode(&newTodo)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if todo, ok := todoMap[id]; ok {
+		todo.Name = newTodo.Name
+		todo.Completed = newTodo.Completed
+		Rd.JSON(w, http.StatusOK, Success{true})
+	} else {
+		Rd.JSON(w, http.StatusBadRequest, Success{false})
+	}
+}
+func DeleteTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if _, ok := todoMap[id]; ok {
+		delete(todoMap, id)
+		Rd.JSON(w, http.StatusOK, Success{true})
+	} else {
+		Rd.JSON(w, http.StatusNotFound, Success{false})
+	}
+}
 
 func MakeRootHandler() http.Handler {
 	todoMap = make(map[int]Todo)
